308616a14439577922a293cc931a2dc00a644d9d
Robert Abreu <robert.abreu3@gmail.com>
2022-10-15 21:22:22 -0400

    fixy module import

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
-import { Fragment, useState } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline/index.js';
 
 
 type NavProps = {
