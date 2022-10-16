1580382a8e4b53f8e554e83dab56e1b0c1bb1dff
Robert Abreu <robert.abreu3@gmail.com>
2022-10-15 21:14:15 -0400

    added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://econicdev.github.io',
+  base:'/atsro-template'
 });
