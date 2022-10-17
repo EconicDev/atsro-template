9920c3f13ba7a77a94fa787173ff2529550236c8
Robert Abreu <robert.abreu3@gmail.com>
2022-10-16 20:41:25 -0400

    move common components to file folder

---
+++ src/components/Stateless.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const Header2 = ({ children }) => <h2 class="text-4xl text-center">{children}</h2>
+export const MissionParagraph = ({ children }) => <p class="text-justify p-4">{children}</p>
