a20f7c806325f800511047ed24d0d12ad7b8826e
Robert Abreu <robert.abreu3@gmail.com>
2022-10-20 21:27:53 -0400

    ensure content was mobile friendly

--- src/components/Stateless.tsx
+++ src/components/Stateless.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 /* About Us*/
 export const Header2 = ({ children }) => <h2 class="text-4xl text-center">{children}</h2>
-export const MissionParagraph = ({ children }) => <p class="text-justify p-4">{children}</p>
+export const MissionParagraph = ({ children }) => <p class="text-left sm:text-justify p-4">{children}</p>
 
 export const ArticleTitle = ({ children }) => <h2 class="text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">{children}</h2>
 export const SectionTitle = ({ children }) => <h3 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{children}</h3>
