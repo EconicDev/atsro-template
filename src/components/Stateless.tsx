36370972be21bbf320de1bc6af27e4d5dd0b2158
Robert Abreu <robert.abreu3@gmail.com>
2022-10-20 23:42:58 -0400

    added more content to services

--- src/components/Stateless.tsx
+++ src/components/Stateless.tsx
@@ -5,4 +5,4 @@ export const Header2 = ({ children }) => <h2 className="text-4xl text-center">{c
 export const MissionParagraph = ({ children }) => <p className="text-left sm:text-justify p-4">{children}</p>
 
 export const ArticleTitle = ({ children }) => <h2 className="text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">{children}</h2>
-export const SectionTitle = ({ children }) => <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{children}</h3>
+export const SectionTitle = ({ children }) => <h3 className="text-2xl text-left font-bold tracking-tight text-gray-900 sm:text-3xl">{children}</h3>
